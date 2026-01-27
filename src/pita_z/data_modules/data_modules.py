ac7ff8e50843c9e1ee0eb64fadef04df8881663f
ashodkh <ask126@pitt.edu>
2026-01-27 09:50:18 -0800

    added calpit images dataset and data module

--- src/pita_z/data_modules/data_modules.py
+++ src/pita_z/data_modules/data_modules.py
@@ -179,4 +179,128 @@ class CalpitPhotometryDataModule(pl.LightningDataModule):
 
     def val_dataloader(self) -> DataLoader:
         return DataLoader(self.ds_val, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, persistent_workers=True, pin_memory=torch.cuda.is_available())
-        
+
+class CalpitImagesDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        path_file: str = None,
+        pit: np.ndarray = None,
+        with_redshift: bool = False,
+        with_features: bool = False,
+        with_weights: bool = False,
+        reddening_transform = None,
+        load_ebv: bool = False,
+        label_f: int = 1
+    ):
+        super().__init__()
+        if Path(path_file).suffix == '.hdf5':
+            self.file = h5py.File(path_file, 'r')
+        self.pit = pit
+        self.with_redshift = with_redshift
+        self.with_features = with_features
+        self.with_weights = with_weights
+        self.reddening_transform = reddening_transform
+        self.load_ebv = load_ebv
+        self.label_f = label_f
+
+    def __len__(self) -> int:
+        return len(self.h5_file['images'])
+    
+    def __getitem__(self, idx: int):
+        image = self.h5_file['images'][idx]
+        ebv = self.h5_file['ebvs'][idx] if self.load_ebv else None
+        redshift = self.h5_file['redshifts'][idx] if self.with_redshift else None
+        color_features = self.h5_file['dered_color_features'][idx] if self.with_features else 1
+        redshift_weight = self.h5_file[f'use_redshift_{self.label_f}'][idx] if self.with_weights else 1
+
+        # Apply reddening transformation if provided
+        if self.reddening_transform:
+            image = self.reddening_transform([image, ebv])
+
+        if self.with_redshift:
+            return image.astype(np.float32), self.pit[idx], redshift.astype(np.float32), redshift_weight.astype(np.float32), color_features.astype(np.float32)
+        else:
+            return image.astype(np.float32), self.pit[idx]
+
+class CalpitImagesDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        batch_size: int = 128,
+        num_workers: int = 1,
+        path_train: str = None,
+        pit_train: np.ndarray = None,
+        path_val: str = None,
+        pit_val: np.ndarray = None,
+        with_redshift: bool = False,
+        with_features: bool = False,
+        with_weights: bool = False,
+        reddening_transform = None,
+        load_ebv: bool = False,
+        label_f: int = 1
+    ):
+        super().__init__()
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.path_train = path_train
+        self.pit_train = pit_train
+        self.path_val = path_val
+        self.pit_val = pit_val
+        self.with_redshift = with_redshift
+        self.with_features = with_features
+        self.with_weights = with_weights
+        self.reddening_transform = reddening_transform
+        self.load_ebv = load_ebv
+        self.label_f = label_f
+
+
+    def setup(self, stage):
+        self.images_train = self._create_dataset(
+            self.path_train,
+            self.pit_train,
+            self.label_f
+        )
+        self.images_val = self._create_dataset(
+            self.path_val,
+            self.pit_val,
+            1
+        )
+
+    def _create_dataset(self, path: str, pit: np.ndarray, label_f: int):
+        return CalpitImagesDataset(
+            path_file=path,
+            pit=pit,
+            with_redshift=self.with_redshift,
+            with_features=self.with_features,
+            with_weights=self.with_weights,
+            reddening_transform=self.reddening_transform,
+            load_ebv=self.load_ebv,
+            label_f=label_f
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.images_train,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            persistent_workers=True,
+            pin_memory=torch.cuda.is_available()
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.images_val,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            persistent_workers=True,
+            pin_memory=torch.cuda.is_available()
+        )
+
+
+
+
+
+
+
+    
